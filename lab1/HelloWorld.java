20b165355aa37a6e54a986beacbcb4fd735f31dc
chansgary <63241838+chansgary@users.noreply.github.com>
2021-06-26 20:02:34 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+        System.out.println("This is the first Java program I learned from CS61B.");
+         
+
+    }
+}
